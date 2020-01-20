8ec435d909051f245e92c5e069d5d55105bc1c2b
dbgso <s092018@gmail.com>
2020-01-20 11:19:48 +0900

    Modify: format by prettier

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,6 +1,6 @@
-import { hello } from "../src/index";
+import { hello } from '../src/index';
 describe('hello', () => {
-    it('should be success', () => {
-        expect(hello('World')).toBe('Hello, World!')
-    })
-})
+  it('should be success', () => {
+    expect(hello('World')).toBe('Hello, World!');
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export function hello(name: string): string {
   return `Hello, ${name}!`;
 }
 
-console.log(hello("TypeScript"));
+console.log(hello('TypeScript'));
